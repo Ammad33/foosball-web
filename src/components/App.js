40b968f2be92beaa2d789ba4fbe71f20f666bead
ibmirzaib <ib.mirza.ib@gmail.com>
2020-10-01 03:04:37 +0500

    Grid height added.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='ui grid'>
+    <div style={{ height: 100 + '%' }} className='ui grid'>
       <div className='two wide column'>left sidebar</div>
       <div className='twelve wide column'>main</div>
       <div className='two wide column'>right sidebar</div>
