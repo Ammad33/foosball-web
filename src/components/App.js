a1cc20974684677d10bee5af69d0b2b132caaf64
ibmirzaib <ib.mirza.ib@gmail.com>
2020-10-01 02:46:00 +0500

    Removed extra code. Added semantic UI library. Working on project structure.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='ui grid'>
+      <div className='two wide column'>left sidebar</div>
+      <div className='twelve wide column'>main</div>
+      <div className='two wide column'>right sidebar</div>
+    </div>
+  );
+}
+
+export default App;
