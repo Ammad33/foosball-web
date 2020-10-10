0b57fcb295239e9025f1303ff1307c15caf78748
ibmirzaib <ib.mirza.ib@gmail.com>
2020-10-10 17:23:06 +0500

    Changes for design and style changes.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MuiTheme from './hoc/Theme';
-import './index.scss';
+import './index.module.scss';
 
 ReactDOM.render(
   <React.StrictMode>
