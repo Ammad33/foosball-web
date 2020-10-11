58612b1608f5a687ae40e05ce92214f3098351f0
ibmirzaib <ib.mirza.ib@gmail.com>
2020-10-11 22:23:05 +0500

    Fixed git issues.

--- src/containers/Campaigns/index.js
+++ src/containers/Campaigns/index.js
@@ -1,10 +1,6 @@
 import React, { useContext } from 'react';
 import { RootContext } from '../../context/RootContext';
-<<<<<<< HEAD
-import { Grid } from '@material-ui/core';
-=======
-import {Grid,Container} from '@material-ui/core';
->>>>>>> b2a3489583e2372767282e54e47fcaafc60fbf2f
+import { Grid, Container } from '@material-ui/core';
 import CampaignsCard from './CampaignsCard';
 
 const Campaigns = () => {
@@ -12,23 +8,16 @@ const Campaigns = () => {
 
   return (
     <Container>
-      
       <Grid container spacing={2}>
-<<<<<<< HEAD
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_) => {
           return (
-            <Grid item xs={12} sm={6} md={4}>
-=======
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_) => {
-        return(
-          <Grid item xs={12} sm={12} md={4}> 
->>>>>>> b2a3489583e2372767282e54e47fcaafc60fbf2f
+            <Grid item xs={12} sm={12} md={4}>
               <CampaignsCard />
             </Grid>
           );
         })}
       </Grid>
-      </Container>
+    </Container>
   );
 };
 
