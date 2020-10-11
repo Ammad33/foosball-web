b2a3489583e2372767282e54e47fcaafc60fbf2f
aqeeldilmir <nabeel.afzal3392@gmail.com>
2020-10-11 10:12:44 -0700

    changes in container

--- src/containers/Campaigns/index.js
+++ src/containers/Campaigns/index.js
@@ -1,22 +1,23 @@
 import React, { useContext } from 'react';
 import { RootContext } from '../../context/RootContext';
-import {Grid} from '@material-ui/core';
+import {Grid,Container} from '@material-ui/core';
 import CampaignsCard from './CampaignsCard';
 
 const Campaigns = () => {
   const { authToken } = useContext(RootContext);
 
   return (
-    <div>
+    <Container>
+      
       <Grid container spacing={2}>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_) => {
         return(
-          <Grid item xs={12} sm={6} md={4}> 
+          <Grid item xs={12} sm={12} md={4}> 
               <CampaignsCard />
         </Grid>)
       })}
       </Grid>
-    </div>
+      </Container>
   );
 };
 
