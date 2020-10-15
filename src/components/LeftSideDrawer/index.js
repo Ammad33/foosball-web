cda556eb12eaeb65f6f572acdaab8025c1c60a06
Shehbaz <shehbazbashir@gmail.com>
2020-10-15 16:14:13 +0100

    added svg changes

--- src/components/LeftSideDrawer/index.js
+++ src/components/LeftSideDrawer/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import ListItem from './ListItem';
 import Campaigns from '../../assets/Campaigns.svg';
+import SVG from 'react-inlinesvg';
+
+
+//import Prospects from '../../assets/Prospects.svg';
 import AssignmentSharpIcon from '@material-ui/icons/AssignmentSharp';
 
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
@@ -8,28 +12,33 @@ import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
 import OfflineBoltIcon from '@material-ui/icons/OfflineBolt';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import BarChartIcon from '@material-ui/icons/BarChart';
-
+const IconProspects = () => {return <SVG src={require('../../assets/Prospects.svg')} />};
+const IconCampaign = () => {return <SVG src={require('../../assets/Campaigns.svg')} />};
+const IconMessages = () => {return <SVG src={require('../../assets/Messages.svg')} />};
+const IconReports = () => {return <SVG src={require('../../assets/Reports.svg')} />};
+const IconSettings = () => {return <SVG src={require('../../assets/settings.svg')} />};
+const IconWallet = () => {return <SVG src={require('../../assets/Wallet.svg')} />};
 const LeftSideDrawer = () => {
   const [active, setActive] = useState('Campaign');
 
   return (
     <>
       <ListItem
-        icon={<AssignmentSharpIcon fontSize='small' />}
+        icon={<IconCampaign />}
         active={active === 'Campaign' ? true : false}
         title={'Campaigns'}
         onClick={() => setActive('Campaign')}
       />
 
       <ListItem
-        icon={<OfflineBoltIcon fontSize='small' />}
+        icon={<IconProspects/>}
         active={active === 'Prospects' ? true : false}
-        title={'Prospects'}
+        title={'Prospects'}n
         onClick={() => setActive('Prospects')}
       />
 
       <ListItem
-        icon={<BarChartIcon fontSize='small' />}
+        icon={<IconReports />}
         active={active === 'Reports' ? true : false}
         title={'Reports'}
         onClick={() => setActive('Reports')}
@@ -37,7 +46,7 @@ const LeftSideDrawer = () => {
       />
 
       <ListItem
-        icon={<MailOutlineIcon fontSize='small' />}
+        icon={<IconMessages />}
         active={active === 'Messages' ? true : false}
         onClick={() => setActive('Messages')}
         title={'Messages'}
@@ -51,7 +60,7 @@ const LeftSideDrawer = () => {
       />
 
       <ListItem
-        icon={<SettingsSharpIcon fontSize='small' />}
+        icon={<IconSettings />}
         active={active === 'Settings' ? true : false}
         title={'Settings'}
         onClick={() => setActive('Settings')}
