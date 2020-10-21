3b4ad3d9a84e549b2ab8b55c181993dcd7dd0a41
ibmirzaib <ib.mirza.ib@gmail.com>
2020-10-21 16:13:28 +0500

    stepper added.

--- src/containers/AddCampaign/index.js
+++ src/containers/AddCampaign/index.js
@@ -1,24 +1,79 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
 import { Dialog } from '@material-ui/core';
 
+const QontoConnector = withStyles({
+  alternativeLabel: {
+    top: 10,
+    left: 'calc(-50% + 16px)',
+    right: 'calc(50% + 16px)',
+  },
+  active: {
+    '& $line': {
+      borderColor: '#784af4',
+    },
+  },
+  completed: {
+    '& $line': {
+      borderColor: '#784af4',
+    },
+  },
+  line: {
+    borderColor: '#eaeaf0',
+    borderTopWidth: 10,
+    borderRadius: 0,
+  },
+})(StepConnector);
+function QontoStepIcon(props) {
+  return '';
+}
 const AddCampaign = ({ open }) => {
-
-    return (
-        <Dialog
-            disableBackdropClick
-            disableEscapeKeyDown
-            maxWidth="xs"
-            aria-labelledby="confirmation-dialog-title"
-            open={open}
-
-        >
-            <div style={{
-                height: '926px',
-                width: '1345px'
-            }}>
-            </div>
-        </Dialog>
-    )
+  const steps = [
+    'Campaign Details',
+    'Add Team Members',
+    'Budget & Conversion Goal',
+    'Create Collection',
+    'Deliverables',
+    'Compensation',
+    'Negotiables',
+    'Choose Influencer',
+    'Review And Send',
+  ];
+  const activeStep = 1;
+  return (
+    <Dialog
+      disableBackdropClick
+      disableEscapeKeyDown
+      maxWidth='xs'
+      aria-labelledby='confirmation-dialog-title'
+      open={open}
+    >
+      <div
+        style={{
+          height: '926px',
+          width: '1345px',
+        }}
+      >
+        <div>
+          <Stepper
+            alternativeLabel
+            activeStep={activeStep}
+            connector={<QontoConnector />}
+          >
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel StepIconComponent={QontoStepIcon}></StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </div>
+      </div>
+    </Dialog>
+  );
 };
 
 export default AddCampaign;
