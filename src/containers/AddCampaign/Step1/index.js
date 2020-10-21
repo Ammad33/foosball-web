8e552fa080f3e969ebd3bba02f95d1752af1dced
aqeeldilmir <aqeeldilmir@gmail.com>
2020-10-21 11:46:20 -0700

    chnages

--- src/containers/AddCampaign/Step1/index.js
+++ src/containers/AddCampaign/Step1/index.js
@@ -4,21 +4,30 @@ import { Grid, TextField } from '@material-ui/core';
 
 const Step1 = () => {
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} style={{ padding: '24px' }}>
             <Grid item md={12}>
-                <TextField id="outlined-basic" label="Campaign Name" variant="outlined" />
+                <TextField id="outlined-basic" fullWidth label="Campaign Name" variant="outlined" />
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
-                <TextField id="outlined-basic" label="Start Date" variant="outlined" />
+                <TextField id="outlined-basic" fullWidth label="Start Date" variant="outlined" />
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
-                <TextField id="outlined-basic" label="End Date" variant="outlined" />
+                <TextField id="outlined-basic" fullWidth label="End Date" variant="outlined" />
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
-                <TextField id="outlined-basic" label="Start Time" variant="outlined" />
+                <TextField id="outlined-basic" fullWidth label="Start Time" variant="outlined" />
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
-                <TextField id="outlined-basic" label="End Time" variant="outlined" />
+                <TextField id="outlined-basic" fullWidth label="End Time" variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+                <TextField id="outlined-basic" fullWidth label="Promotion Discount" variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+                <TextField id="outlined-basic" fullWidth label="Percentage" variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+                <TextField id="outlined-basic" fullWidth multiline rows={4} placeholder={"Enter a custom message to send with your invitation"} variant="outlined" />
             </Grid>
 
         </Grid>
