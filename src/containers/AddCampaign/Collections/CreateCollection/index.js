8b1597768d16026e2462b481d3a7e04a54540f5c
aqeeldilmir <aqeeldilmir@gmail.com>
2020-10-29 10:33:45 -0700

    changes

---
+++ src/containers/AddCampaign/Collections/CreateCollection/index.js
@@ -0,0 +1,14 @@
+import { Grid } from '@material-ui/core';
+
+const CreateCollection = () => {
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+
+            </Grid>
+        </Grid>
+    );
+
+};
+
+export default CreateCollection;
