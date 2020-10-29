81d9bdb1e19ba3a66d6b144c27d90f8ed47b8915
aqeeldilmir <aqeeldilmir@gmail.com>
2020-10-29 13:46:22 -0700

    add campaigns

---
+++ src/containers/AddCampaign/Deliverables/CreateDeliverable/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import TextField from '../../../components/TextField';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+const CreateDeliverable = () => {
+    return (
+        <Grid container item={3} >
+            <Grid item xs={12}>
+                <TextField
+                    id='outlined-basic'
+                    fullWidth
+                    value={startDate}
+                    onChange={handleStartDate}
+                    label='Deliverable Dead Date'
+                    variant='outlined'
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end"><EventNoteIcon ><KeyboardDatePicker
+                            disableToolbar
+                            variant="inline"
+                            format="MM/dd/yyyy"
+                            margin="normal"
+                            id="date-picker-inline"
+                            label="Date picker inline"
+                            style={{ display: 'none' }}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        /></EventNoteIcon></InputAdornment>,
+                    }}
+                />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default CreateDeliverable;
