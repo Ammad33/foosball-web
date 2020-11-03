d66c96da4bc453656cc78f142db9bfbfa468d889
ibmirzaib <ib.mirza.ib@gmail.com>
2020-11-04 01:07:48 +0500

    Reset password deisgn added.

--- src/containers/ResetPassword/index.js
+++ src/containers/ResetPassword/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import styles from './ResetPassword.module.scss';
+import mainStyles from './../../index.module.scss';
 const ResetPassword = () => {
-  return <div>Reset Password</div>;
+  const [inputType, setInputType] = useState('password');
+  return (
+    <div className={styles.resetPasswordContainer}>
+      <h1>Reset Password</h1>
+      <p>Enter you new password below</p>
+      <TextField
+        id='outlined-basic'
+        label='Password'
+        variant='outlined'
+        type={inputType}
+      />
+      <div className={styles.actionsContainer}>
+        <Button className={mainStyles.defaultButton} variant='contained'>
+          Reset Password
+        </Button>
+        <Button className={mainStyles.defaultOutlinedButton} variant='outlined'>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ResetPassword;
