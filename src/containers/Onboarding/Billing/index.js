0bb714c85c431f37ebdc9b2418eec4596b459120
ibmirzaib <ib.mirza.ib@gmail.com>
2020-11-11 02:05:36 +0500

    active state added. active icon added for paypal option. Active effects added.

--- src/containers/Onboarding/Billing/index.js
+++ src/containers/Onboarding/Billing/index.js
@@ -4,185 +4,231 @@ import TextField from '../../../components/TextField';
 import styles from './Billing.module.scss';
 import { Plus, HelpCircle } from 'react-feather';
 import paypal from '../../../assets/paypal.png';
-
-
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 const Billing = () => {
-    const [primary, setPrimary] = useState(false);
-    const [secondary, setSecondary] = useState(false);
-    return (
-        <div >
-            <p className={styles.billingHeading}>You will be billed automatically every 30 days. See our pricing model for billing info.</p>
-            <p className={styles.primary}>Primary Payment</p>
-            <Grid container spacing={3}>
-                <Grid item xs={6} className={styles.iconItem}>
-                    <button onClick={() => setPrimary(!primary)}>
-                        <Plus /> Add Credit Card
-                </button>
-                </Grid>
-                <Grid item xs={6} className={styles.imageItem}>
-                    <button>
-                        <img src={paypal} height={38} width={152} />
-                        <span>malloryhuntsman@gmail.com</span>
-                    </button>
-                </Grid>
-                {primary &&
-                    <> <Grid item md={12}>
-                        <TextField
-                            id='outlined-basic'
-                            fullWidth
-                            label='Name on Card'
-                            variant='outlined'
-                        />
-
-                    </Grid>
-                        <Grid item md={12}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='Card Number'
-                                variant='outlined'
-                            />
-
-                        </Grid>
-                        <Grid item md={6}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='Expiration Date'
-                                variant='outlined'
-                            />
-                        </Grid>
-                        <Grid item md={6}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='CCV'
-                                variant='outlined'
-                            />
-                        </Grid>
-                        <Grid item md={12}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='Street Address'
-                                variant='outlined'
-                            />
-                        </Grid>
-                        <Grid item md={6}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='City'
-                                variant='outlined'
-                            />
-                        </Grid>
-                        <Grid item md={2}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='State'
-                                variant='outlined'
-                            />
-                        </Grid>
-                        <Grid item md={4}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='Zip Code'
-                                variant='outlined'
-                            />
-                        </Grid>
-                    </>
-                }
-            </Grid >
-            <div>
-                <p className={styles.primary}>Secondary Payment <HelpCircle /></p>
-                <Grid container spacing={3}>
-                    <Grid item xs={6} className={styles.iconItem}>
-                        <button onClick={() => setSecondary(!secondary)}>
-                            <Plus /> Add Credit Card
-                </button>
-                    </Grid>
-                    <Grid item xs={6} className={styles.imageItem}>
-                        <button>
-                            <img src={paypal} height={38} width={152} />
-                            <span>malloryhuntsman@gmail.com</span>
-                        </button>.
-            </Grid>
-                    {secondary &&
-                        <> <Grid item md={12}>
-                            <TextField
-                                id='outlined-basic'
-                                fullWidth
-                                label='Name on Card'
-                                variant='outlined'
-                            />
-
-                        </Grid>
-                            <Grid item md={12}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='Card Number'
-                                    variant='outlined'
-                                />
-
-                            </Grid>
-                            <Grid item md={6}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='Expiration Date'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                            <Grid item md={6}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='CCV'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                            <Grid item md={12}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='Street Address'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                            <Grid item md={6}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='City'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                            <Grid item md={2}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='State'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                            <Grid item md={4}>
-                                <TextField
-                                    id='outlined-basic'
-                                    fullWidth
-                                    label='Zip Code'
-                                    variant='outlined'
-                                />
-                            </Grid>
-                        </>
-                    }
-                </Grid>
+  const [primary, setPrimary] = useState(false);
+  const [primaryBillingType, setPrimaryBillingType] = useState(null);
+  const [secondaryBillingType, setSecondaryBillingType] = useState(null);
+  const [secondary, setSecondary] = useState(false);
+  function handlePrimaryBilling(type) {
+    // if (type === 'CC' && primaryBillingType === 'CC') {
+    //   setPrimaryBillingType(null);
+    // } else {
+    setPrimaryBillingType(type);
+    // }
+  }
+  function handleSecondaryBilling(type) {
+    // if (type === 'CC' && primaryBillingType === 'CC') {
+    //   setPrimaryBillingType(null);
+    // } else {
+    setSecondaryBillingType(type);
+    // }
+  }
+  return (
+    <div>
+      <p className={styles.billingHeading}>
+        You will be billed automatically every 30 days. See our pricing model
+        for billing info.
+      </p>
+      <p className={styles.primary}>Primary Payment</p>
+      <Grid container spacing={3}>
+        <Grid item xs={6} className={styles.iconItem}>
+          <button
+            className={clsx(primaryBillingType === 'CC' ? styles.active : '')}
+            onClick={() => handlePrimaryBilling('CC')}
+          >
+            <Plus /> Add Credit Card
+          </button>
+        </Grid>
+        <Grid item xs={6} className={styles.imageItem}>
+          <button
+            className={clsx(primaryBillingType === 'PP' ? styles.active : '')}
+            onClick={() => handlePrimaryBilling('PP')}
+          >
+            <div className={styles.content}>
+              <div>
+                <img src={paypal} height={38} width={152} />
+                <span>malloryhuntsman@gmail.com</span>
+              </div>
+              {primaryBillingType === 'PP' ? <CheckCircleIcon /> : ''}
             </div>
-        </div >
-    );
-
+          </button>
+        </Grid>
+        {primaryBillingType === 'CC' && (
+          <>
+            {' '}
+            <Grid item md={12}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='Name on Card'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={12}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='Card Number'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='Expiration Date'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='CCV'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={12}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='Street Address'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='City'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={2}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='State'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item md={4}>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                label='Zip Code'
+                variant='outlined'
+              />
+            </Grid>
+          </>
+        )}
+      </Grid>
+      <div>
+        <p className={styles.primary}>
+          Secondary Payment <HelpCircle />
+        </p>
+        <Grid container spacing={3}>
+          <Grid item xs={6} className={styles.iconItem}>
+            <button
+              className={clsx(
+                secondaryBillingType === 'CC' ? styles.active : ''
+              )}
+              onClick={() => handleSecondaryBilling('CC')}
+            >
+              <Plus /> Add Credit Card
+            </button>
+          </Grid>
+          <Grid item xs={6} className={styles.imageItem}>
+            <button
+              className={clsx(
+                secondaryBillingType === 'PP' ? styles.active : ''
+              )}
+              onClick={() => handleSecondaryBilling('PP')}
+            >
+              <div className={styles.content}>
+                <div>
+                  <img src={paypal} height={38} width={152} />
+                  <span>malloryhuntsman@gmail.com</span>
+                </div>
+                {secondaryBillingType === 'PP' ? <CheckCircleIcon /> : ''}
+              </div>
+            </button>
+          </Grid>
+          {secondaryBillingType === 'CC' && (
+            <>
+              {' '}
+              <Grid item md={12}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='Name on Card'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={12}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='Card Number'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={6}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='Expiration Date'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={6}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='CCV'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={12}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='Street Address'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={6}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='City'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={2}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='State'
+                  variant='outlined'
+                />
+              </Grid>
+              <Grid item md={4}>
+                <TextField
+                  id='outlined-basic'
+                  fullWidth
+                  label='Zip Code'
+                  variant='outlined'
+                />
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Billing;
