43359f492eac3d39405cdb0bd5c0c70d1f42e2d1
aqeeldilmir <aqeeldilmir@gmail.com>
2020-11-24 02:20:40 +0500

    'dev conflicts'

--- src/containers/AddCampaign/index.js
+++ src/containers/AddCampaign/index.js
@@ -810,30 +810,16 @@ const AddCampaign = ({ open, handleCancel, step, campaign }) => {
       setActiveNext(false);
     }
   };
-<<<<<<< HEAD
-
-=======
->>>>>>> 01f633f423a5a45017b429e9ed88fe75218d86de
 
   const createCampaign = async () => {
-    debugger;
+
     try {
-<<<<<<< HEAD
-      if (discountType === "Amount") {
-        typ = "FLAT"
-        val = "{\"amount\":{\"amount\": \"" + discount + "\",\"currency\":\"USD\"}}"
-      }
-      else {
-        typ = "PERCENTAGE"
-        val = "{\"percentage\":\"" + discount + "\"}"
-=======
       if (discountType === 'Amount') {
         typ = 'FLAT';
         val = '{"amount":{"amount": "' + discount + '","currency":"USD"}}';
       } else {
         typ = 'PERCENTAGE';
         val = '{"percentage":"' + discount + '"}';
->>>>>>> 01f633f423a5a45017b429e9ed88fe75218d86de
       }
       await API.graphql(
         graphqlOperation(
@@ -848,23 +834,14 @@ const AddCampaign = ({ open, handleCancel, step, campaign }) => {
         }
         `,
           {
-<<<<<<< HEAD
-
-=======
->>>>>>> 01f633f423a5a45017b429e9ed88fe75218d86de
             input: {
               brandId: '8ece73cc-3079-4f45-b7bb-4f6007c8344d',
               name: campaignName,
               startDate: Date.parse(`${startDate} ${startTime}`) / 1000,
               endDate: Date.parse(`${endDate} ${endTime}`) / 1000,
               discount: { value: val, type: typ },
-<<<<<<< HEAD
-              budget: { amount: budget, currency: "USD" },
-              targetGrossSales: { amount: targetGrossSale, currency: "USD" }
-=======
               budget: { amount: budget, currency: 'USD' },
               targetGrossSales: { amount: targetGrossSale, currency: 'USD' },
->>>>>>> 01f633f423a5a45017b429e9ed88fe75218d86de
             },
           }
         )
