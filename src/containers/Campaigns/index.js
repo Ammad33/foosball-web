b6fe5b565e1914e7e8002952c44474ded603fc8f
aqeeldilmir <aqeeldilmir@gmail.com>
2020-12-19 01:31:31 +0500

    update password

--- src/containers/Campaigns/index.js
+++ src/containers/Campaigns/index.js
@@ -11,29 +11,29 @@ import SVG from 'react-inlinesvg';
 import { RootContext } from '../../context/RootContext';
 
 const IconCampaign = () => {
-	return <SVG src={require('../../assets/Campaigns_large.svg')} />;
+  return <SVG src={require('../../assets/Campaigns_large.svg')} />;
 };
 
 const Campaigns = () => {
-	const history = useHistory();
-	const [active, setActive] = useState('ALL');
-	const [campaigns, setCampaigns] = useState([]);
-	const [bkupCampaigns, setBkupCampaigns] = useState([]);
-	const [addCampaign, setAddCampagin] = useState(false);
-	const [meData, setMeData] = useState([]);
-	const [brandName, setBrandName] = useState([]);
-	const { brandId, setBrands, searchValue, brandType } = useContext(RootContext);
+  const history = useHistory();
+  const [active, setActive] = useState('ALL');
+  const [campaigns, setCampaigns] = useState([]);
+  const [bkupCampaigns, setBkupCampaigns] = useState([]);
+  const [addCampaign, setAddCampagin] = useState(false);
+  const [meData, setMeData] = useState([]);
+  // const [brandName, setBrandName] = useState([]);
+  const { brandId, setBrands, searchValue, brandType, setInfluencers, setBrandIdd, setBrandName } = useContext(RootContext);
 
-	useEffect(() => {
-		if (!brandId || brandId === '') {
-			getMeData();
-		}
-	}, []);
+  useEffect(() => {
+    if (!brandId || brandId === '') {
+      getMeData();
+    }
+  }, []);
 
-	const getMeData = async () => {
-		try {
-			const mydata = await API.graphql({
-				query: `{
+  const getMeData = async () => {
+    try {
+      const mydata = await API.graphql({
+        query: `{
 						me {
 							email
 							fullName
@@ -64,19 +64,36 @@ const Campaigns = () => {
 							phoneNumber
 						}
 				}`,
-			});
+      });
 
-			setBrands(mydata.data.me.organizations);
-			setMeData(mydata.data.me.organizations);
-			setBrandName(mydata.data.me.organizations[0].organization.__typename);
-		} catch (e) {
-			console.log(e);
-		}
-	};
-	const getCampaigns = async () => {
-		try {
-			const campaigns = await API.graphql({
-				query: `{
+      let brandsData = [];
+      let influencersData = [];
+      mydata.data.me.organizations !== null && mydata.data.me.organizations.forEach(item => {
+        if (item.organization.__typename == "Influencer") {
+          influencersData.push(item);
+        } else if (item.organization.__typename === "Brand") {
+          brandsData.push(item);
+        }
+      })
+      setBrands(brandsData);
+      setInfluencers(influencersData);
+      if (brandsData.length > 0) {
+        setBrandName(brandsData[0].organization.name);
+        setBrandIdd(brandsData[0].organization.id);
+      } else if (influencersData.length > 0) {
+        setBrandName(influencersData[0].organization.name);
+        setBrandIdd(influencersData[0].organization.id);
+
+      }
+      setMeData(mydata.data.me.organizations);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const getCampaigns = async () => {
+    try {
+      const campaigns = await API.graphql({
+        query: `{
         campaigns(brandId: "${brandId}") {
           campaigns {
             name
@@ -88,177 +105,176 @@ const Campaigns = () => {
           }
         }
       }`,
-			});
-			setCampaigns(campaigns.data.campaigns.campaigns);
-			setBkupCampaigns(campaigns.data.campaigns.campaigns);
-		} catch (e) { }
-	};
+      });
+      setCampaigns(campaigns.data.campaigns.campaigns);
+      setBkupCampaigns(campaigns.data.campaigns.campaigns);
+    } catch (e) { }
+  };
 
-	useEffect(() => {
-		if (brandId !== '') {
-			setCampaigns([]);
-			getCampaigns();
-		}
-	}, [brandId, addCampaign]);
+  useEffect(() => {
+    if (brandId !== '') {
+      setCampaigns([]);
+      getCampaigns();
+    }
+  }, [brandId, addCampaign]);
 
-	useEffect(() => {
-		searchCampaigns();
-	}, [searchValue]);
+  useEffect(() => {
+    searchCampaigns();
+  }, [searchValue]);
 
-	const searchCampaigns = () => {
-		let copiedCampaigns = [...bkupCampaigns];
-		if (searchValue.trim()) {
-			copiedCampaigns = copiedCampaigns.filter((campaign) => {
-				return (
-					campaign.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
-				);
-			});
-		}
-		setCampaigns(copiedCampaigns);
-	};
+  const searchCampaigns = () => {
+    let copiedCampaigns = [...bkupCampaigns];
+    if (searchValue.trim()) {
+      copiedCampaigns = copiedCampaigns.filter((campaign) => {
+        return (
+          campaign.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
+        );
+      });
+    }
+    setCampaigns(copiedCampaigns);
+  };
 
-	const handleDelete = async (campaignId) => {
+  const handleDelete = async (campaignId) => {
 
-		try {
-			await API.graphql(
-				graphqlOperation(`mutation deleteCampaign($brandId: ID!, $id: ID!) {
+    try {
+      await API.graphql(
+        graphqlOperation(`mutation deleteCampaign($brandId: ID!, $id: ID!) {
           deleteCampaign(brandId: $brandId, id:$id)
         }`, {
-					brandId: brandId,
-					id: campaignId
-				})
-			);
-			getCampaigns();
-		} catch (e) {
-			console.log('delete campaign error ', e);
-		}
-	}
-	debugger;
+          brandId: brandId,
+          id: campaignId
+        })
+      );
+      getCampaigns();
+    } catch (e) {
+      console.log('delete campaign error ', e);
+    }
+  }
 
-	return (
-		<>
-			{addCampaign && (
-				<AddCampaign
-					open={addCampaign}
-					handleCancel={() => setAddCampagin(false)}
-					brandId={brandId}
-				/>
-			)}
-			<div className={styles.campaignsContainer}>
-				<div className={styles.CampaignHeadingContainer}>
-					<div className={styles.CampaignHeading}>
-						<span>Campaigns</span>
-						<p>
-							Most recent <ExpandMoreIcon fontSize='small' />
-						</p>
-					</div>
-					{brandType === 'Brand' ? (
-						<button onClick={() => setAddCampagin(true)}>
-							<AddIcon /> New Campaign
-						</button>) : ('')}
+  return (
+    <>
+      {addCampaign && (
+        <AddCampaign
+          open={addCampaign}
+          handleCancel={() => setAddCampagin(false)}
+          brandId={brandId}
+        />
+      )}
+      <div className={styles.campaignsContainer}>
+        <div className={styles.CampaignHeadingContainer}>
+          <div className={styles.CampaignHeading}>
+            <span>Campaigns</span>
+            <p>
+              Most recent <ExpandMoreIcon fontSize='small' />
+            </p>
+          </div>
+          {brandType === 'Brand' ? (
+            <button onClick={() => setAddCampagin(true)}>
+              <AddIcon /> New Campaign
+            </button>) : ('')}
 
-				</div>
-				<div className={styles.CampaignHeadingButton}>
-					<button
-						className={active === 'ALL' ? styles.allActive : ''}
-						onClick={() => setActive('ALL')}
-					>
-						All
+        </div>
+        <div className={styles.CampaignHeadingButton}>
+          <button
+            className={active === 'ALL' ? styles.allActive : ''}
+            onClick={() => setActive('ALL')}
+          >
+            All
           </button>
-					{brandType === 'Brand' ? (
-						<button
-							className={active === 'DRAFT' ? styles.draftActive : ''}
-							onClick={() => setActive('DRAFT')}
-						>
-							Draft
-						</button>
-					) : (
-							<button
-								className={active === "INVITE" ? styles.inviteActive : ""}
-								onClick={() => setActive("INVITE")}
-							>
-								Invite
-							</button>
-						)}
-					<button
-						className={active === 'PENDING' ? styles.pendingActive : ''}
-						onClick={() => setActive('PENDING')}
-					>
-						Pending
+          {brandType === 'Brand' ? (
+            <button
+              className={active === 'DRAFT' ? styles.draftActive : ''}
+              onClick={() => setActive('DRAFT')}
+            >
+              Draft
+            </button>
+          ) : (
+              <button
+                className={active === "INVITE" ? styles.inviteActive : ""}
+                onClick={() => setActive("INVITE")}
+              >
+                Invite
+              </button>
+            )}
+          <button
+            className={active === 'PENDING' ? styles.pendingActive : ''}
+            onClick={() => setActive('PENDING')}
+          >
+            Pending
           </button>
-					<button
-						className={active === 'LIVE' ? styles.liveActive : ''}
-						onClick={() => setActive('LIVE')}
-					>
-						Live
+          <button
+            className={active === 'LIVE' ? styles.liveActive : ''}
+            onClick={() => setActive('LIVE')}
+          >
+            Live
           </button>
-					<button
-						className={active === 'CLOSED' ? styles.closedActive : ''}
-						onClick={() => setActive('CLOSED')}
-					>
-						Closed
+          <button
+            className={active === 'CLOSED' ? styles.closedActive : ''}
+            onClick={() => setActive('CLOSED')}
+          >
+            Closed
           </button>
-					{brandType === 'Brand' ? (
-						<button
-							className={active === 'LAST' ? styles.lostActive : ''}
-							onClick={() => setActive('LAST')}
-						>
-							Lost
-						</button>
-					) : (
-							<button
-								className={active === "DECLINED" ? styles.declinedActive : ""}
-								onClick={() => setActive("DECLINED")}
-							>
-								Declined
-							</button>
-						)}
-				</div>
-				{campaigns.length == 0 ? (
-					<Grid
-						container
-						spacing={0}
-						direction='column'
-						alignItems='center'
-						justify='center'
-						style={{ paddingTop: '15%' }}
-					>
-						<Grid item xs={12}>
-							<IconCampaign />
-						</Grid>
-						<Grid item xs={12}>
-							<div className={styles.noCampaignYet}>No Campaigns Yet</div>
-						</Grid>
-						<Grid item xs={12}>
-							<div className={styles.noCampaignYetHelper}>
-								Click on the button New Campaign button to get started creating
-								a campaign.
+          {brandType === 'Brand' ? (
+            <button
+              className={active === 'LAST' ? styles.lostActive : ''}
+              onClick={() => setActive('LAST')}
+            >
+              Lost
+            </button>
+          ) : (
+              <button
+                className={active === "DECLINED" ? styles.declinedActive : ""}
+                onClick={() => setActive("DECLINED")}
+              >
+                Declined
+              </button>
+            )}
+        </div>
+        {campaigns.length == 0 ? (
+          <Grid
+            container
+            spacing={0}
+            direction='column'
+            alignItems='center'
+            justify='center'
+            style={{ paddingTop: '15%' }}
+          >
+            <Grid item xs={12}>
+              <IconCampaign />
+            </Grid>
+            <Grid item xs={12}>
+              <div className={styles.noCampaignYet}>No Campaigns Yet</div>
+            </Grid>
+            <Grid item xs={12}>
+              <div className={styles.noCampaignYetHelper}>
+                Click on the button New Campaign button to get started creating
+                a campaign.
               </div>
-						</Grid>
-					</Grid>
-				) : (
-						''
-					)}
-				<Grid container spacing={3}>
-					{campaigns.length > 0 &&
-						campaigns.map((campaign) => {
-							if (campaign.status !== active && active !== 'ALL') {
-								return null;
-							}
-							return (
-								<Grid
-									className={styles.gridItem}
-									item
-									key={campaign.id}
-								>
-									<CampaignsCard campaign={campaign} onClick={() => history.push(`/campaignDetail/${campaign.id}`)} handleDelete={handleDelete} />
-								</Grid>
-							);
-						})}
-				</Grid>
-			</div>
-		</>
-	);
+            </Grid>
+          </Grid>
+        ) : (
+            ''
+          )}
+        <Grid container spacing={3}>
+          {campaigns.length > 0 &&
+            campaigns.map((campaign) => {
+              if (campaign.status !== active && active !== 'ALL') {
+                return null;
+              }
+              return (
+                <Grid
+                  className={styles.gridItem}
+                  item
+                  key={campaign.id}
+                >
+                  <CampaignsCard campaign={campaign} onClick={() => history.push(`/campaignDetail/${campaign.id}`)} handleDelete={handleDelete} />
+                </Grid>
+              );
+            })}
+        </Grid>
+      </div>
+    </>
+  );
 };
 
 export default Campaigns;
