20801c22edc77558f272ba8eb05a3a15bc3b9a5f
ibmirzaib <ib.mirza.ib@gmail.com>
2020-12-20 15:22:04 +0500

    Code Refactoring.

--- src/containers/Team/AddMember/index.js
+++ src/containers/Team/AddMember/index.js
@@ -1,49 +1,32 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dialog, Grid } from '@material-ui/core';
 import styles from './AddMember.module.scss';
 import TextField from '../../../components/TextField';
-import { HelpCircle } from 'react-feather';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import SVG from 'react-inlinesvg';
-import { RootContext } from '../../../context/RootContext';
-import DialogActions from '@material-ui/core/DialogActions';
-import Button from '@material-ui/core/Button';
-import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
-import SelectedMembers from './SelectedMembers'
+import Autocomplete, {
+  createFilterOptions,
+} from '@material-ui/lab/Autocomplete';
+import SelectedMembers from './SelectedMembers';
 import { API, graphqlOperation } from 'aws-amplify';
 
-
-const Plus = () => {
-	return (
-		<SVG src={require('../../../assets/pluslarge.svg')} />
-	);
-};
-
 const AddMember = ({ open, closeAdd }) => {
-	const [inviteMember, setInviteMember] = useState({ name: '' });
-	const [newMember, setNewMember] = React.useState({
-		name: ''
-	});
-	const { brands , brandId , roleId } = useContext(RootContext);
-	const [data , setData] = useState([]); 
-	const filter = createFilterOptions();
+  const [inviteMember, setInviteMember] = useState({ name: '' });
+  const [newMember, setNewMember] = React.useState({
+    name: '',
+  });
+  const [data, setData] = useState([]);
+  const filter = createFilterOptions();
 
-	const top100Films = [
-	];
+  const top100Films = [];
 
+  const handleInviteMember = (e, value) => {
+    setInviteMember({
+      name: value.inputValue,
+    });
+  };
 
-	const handleInviteMember = (e, value) => {
-		setInviteMember({
-			name: value.inputValue
-		});
-	}
+  const [value, setValue] = React.useState(null);
 
-	const [value, setValue] = React.useState(null);
-
-
-	const getMeData = async () => {
+  const getMeData = async () => {
     try {
       const mydata = await API.graphql({
         query: `{
@@ -59,136 +42,123 @@ const AddMember = ({ open, closeAdd }) => {
 							}
 						}
 				}`,
-			});
-			// debugger;
+      });
+
       setData(mydata.data.me);
     } catch (e) {
       console.log(e);
     }
   };
 
-
-	const assignRole = async () => {
-		try {
-			const data = {
-				organizationId: "8ece73cc-3079-4f45-b7bb-4f6007c8344d",
-				roleId: "a94b4e45-f72a-4d75-9d31-e4a2cf791775",
-				userId: "b9a59121-fd68-434f-a52c-93036203ee26"
-			};
-			await API.graphql(
-				graphqlOperation(
-					`mutation assignRole($input: AssignRoleInput!) {
+  const assignRole = async () => {
+    try {
+      const data = {
+        organizationId: '8ece73cc-3079-4f45-b7bb-4f6007c8344d',
+        roleId: 'a94b4e45-f72a-4d75-9d31-e4a2cf791775',
+        userId: 'b9a59121-fd68-434f-a52c-93036203ee26',
+      };
+      await API.graphql(
+        graphqlOperation(
+          `mutation assignRole($input: AssignRoleInput!) {
 						assignRole(input: $input) 
       }
       `,
-					{
-						input:data
-					}
-				)
-			);
-		} catch (e) {
-			console.log('Error in mutation for Invite Member ', e);
-		}
-	};
-	debugger;
-	useEffect(() => {
-		getMeData();
-	}, []);
+          {
+            input: data,
+          }
+        )
+      );
+    } catch (e) {
+      console.log('Error in mutation for Invite Member ', e);
+    }
+  };
 
-	return (
-		<Dialog
-			classes={{ paper: styles.addMember }}
-			aria-labelledby='confirmation-dialog-title'
-			open={open}
-			onClose={closeAdd}
-		>
-			<div className={styles.content}>
-				<h6>Add members to your team </h6>
-				<Grid item xs={12} >
-					<Autocomplete
-						value={value}
-						onChange={(event, newValue) => {
-							if (typeof newValue === 'string') {
-								// timeout to avoid instant validation of the dialog's form.
-								setNewMember({
-									name: newValue,
-								});
-							} else if (newValue && newValue.inputValue) {
-								// debugger;
-								handleInviteMember(event, newValue);
-								setNewMember({
-									name: newValue.inputValue,
-								});
-							} else {
-								setValue(newValue);
-							}
-						}}
-						filterOptions={(options, params) => {
-							const filtered = filter(options, params);
+  useEffect(() => {
+    getMeData();
+  }, []);
 
-							if (params.inputValue !== '') {
-								filtered.push({
-									inputValue: params.inputValue,
-								title: `+ "${params.inputValue}"`,
-								});
-							}
+  return (
+    <Dialog
+      classes={{ paper: styles.addMember }}
+      aria-labelledby='confirmation-dialog-title'
+      open={open}
+      onClose={closeAdd}
+    >
+      <div className={styles.content}>
+        <h6>Add members to your team </h6>
+        <Grid item xs={12}>
+          <Autocomplete
+            value={value}
+            onChange={(event, newValue) => {
+              if (typeof newValue === 'string') {
+                // timeout to avoid instant validation of the dialog's form.
+                setNewMember({
+                  name: newValue,
+                });
+              } else if (newValue && newValue.inputValue) {
+                handleInviteMember(event, newValue);
+                setNewMember({
+                  name: newValue.inputValue,
+                });
+              } else {
+                setValue(newValue);
+              }
+            }}
+            filterOptions={(options, params) => {
+              const filtered = filter(options, params);
 
-							return filtered;
-						}}
-						id="free-solo-dialog-demo"
-						options={top100Films}
-						getOptionLabel={(option) => {
-							// e.g value selected with enter, right from the input
-							if (typeof option === 'string') {
-								return option;
-							}
-							if (option.inputValue) {
-								return option.inputValue;
-							}
-							return option.title;
-						}}
-						selectOnFocus
-						clearOnBlur
-						handleHomeEndKeys
-						renderOption={(option) => option.title}
-						style={{ width: 750 }}
-						freeSolo
-						renderInput={(params) => (
-							<TextField {...params} label="Invite members" variant="outlined" />
-						)}
-					/>
-					{/* <TextField
-							id='outlined-basic'
-							fullWidth
-							label='Invite by email'
-							variant='outlined'
-							// value={newInfluencer.fullName}
-							onChange={(e) => handleInviteMember(e.target.value)}
-						// helperText={
-						// 	newInfluencerError.fullName && (
-						// 		<span className={styles.errorText}>Full name is required </span>
-						// 	)
-						// }
-						/> */}
-				</Grid>
-			</div>
-			{inviteMember.name.length > 1 ? (
-				<SelectedMembers
-					TeamMembers={inviteMember} />
-			) : ("")}
-			<div className={styles.footerContainer}>
-				<div className={styles.footer} >
-					<span onClick={closeAdd}>Cancel</span>
-					<div>
-						<button onClick={assignRole}>Invite</button>
-					</div>
-				</div>
-			</div>
+              if (params.inputValue !== '') {
+                filtered.push({
+                  inputValue: params.inputValue,
+                  title: `+ "${params.inputValue}"`,
+                });
+              }
 
-		</Dialog>
-	);
-
+              return filtered;
+            }}
+            id='free-solo-dialog-demo'
+            options={top100Films}
+            getOptionLabel={(option) => {
+              // e.g value selected with enter, right from the input
+              if (typeof option === 'string') {
+                return option;
+              }
+              if (option.inputValue) {
+                return option.inputValue;
+              }
+              return option.title;
+            }}
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            renderOption={(option) => option.title}
+            style={{ width: 750 }}
+            freeSolo
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label='Invite members'
+                variant='outlined'
+              />
+            )}
+          />
+        </Grid>
+      </div>
+      {inviteMember.name.length > 1 ? (
+        <SelectedMembers TeamMembers={inviteMember} />
+      ) : (
+        ''
+      )}
+      <div className={styles.footerContainer}>
+        <div className={styles.footer}>
+          <span onClick={closeAdd}>Cancel</span>
+          <div>
+            <button onClick={assignRole}>Invite</button>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
 };
 
 export default AddMember;
-
--- src/containers/Team/index.js
+++ src/containers/Team/index.js
@@ -1,220 +1,100 @@
 import React, { useState, useEffect, useContext } from 'react';
 import AddIcon from '@material-ui/icons/Add';
-import { InputAdornment, Grid, Avatar, Popover } from '@material-ui/core';
-import { Auth } from 'aws-amplify';
+import { Grid } from '@material-ui/core';
 import styles from './Team.module.scss';
-import { useHistory } from 'react-router-dom';
 import { API } from 'aws-amplify';
 import SVG from 'react-inlinesvg';
 import { RootContext } from '../../context/RootContext';
-import { Plus, MoreVertical, Mail, Edit, Trash } from 'react-feather';
-import TextField from '../../components/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import Divider from '@material-ui/core/Divider';
-import { Link } from 'react-router-dom';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
 import AddMember from './AddMember';
 import TeamData from './TeamData';
 
-
-
-
-const Xcircle = () => {
-	return (
-		<SVG src={require('../../assets/x-circle.svg')} />
-	);
-};
-
-
 const NoUser = () => {
-	return (
-		<SVG src={require('../../assets/noUsers.svg')} />
-	);
+  return <SVG src={require('../../assets/noUsers.svg')} />;
 };
-
-const TeamMembers = [
-	{
-		name: 'Ben Parker',
-		invitationAccepted: false
-	},
-	{
-		name: 'Toby Flenderson',
-		invitationAccepted: false
-	},
-
-	{
-		name: 'Edinson Cavani',
-		invitationAccepted: true
-	},
-	{
-		name: 'Bruno Fernandes',
-		invitationAccepted: true
-	},
-]
 
 const Team = () => {
-	const history = useHistory();
-	const [active, setActive] = useState('ALL');
-	const [members, setMembers] = useState(TeamMembers);
-	const [addMember, setAddMember] = useState(TeamMembers);
-	const [removeMember, setRemoveMember] = useState(TeamMembers);
-	const [teamMember, setTeamMember] = useState([]);
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const [addOpen, setAddOpen] = useState(false);
-	const [editOpen, setEditOpen] = useState(false);
-	const [teams, setTeam] = useState([]);
+  const [addOpen, setAddOpen] = useState(false);
+  const [teams, setTeam] = useState([]);
+  const { brandId } = useContext(RootContext);
 
-	const { brandId } = useContext(RootContext);
-
-
-	// const verifyUser = async () => {
-  //   try {
-  //       const user = await Auth.userAttributes({
-  //         username,
-  //         password,
-  //         attributes: { name },
-  //       });
-  //       setCurrentUser(loggedInUser);
-  //       console.log(user);
-  //       setErrorState(false);
-  //       setLogoutMessage('');
-  //       setErrorMessage('');
-  //       history.push('/onboarding');
-      
-  //   } catch (e) {
-  //     setErrorMessage(e.message);
-  //     setErrorState(true);
-  //     setLogoutMessage('');
-  //   }
-  // };
-
-
-
-	// debugger;
-	const getTeam = async () => {
-		try {
-			const team = await API.graphql({
-				query: `{
+  const getTeam = async () => {
+    try {
+      const team = await API.graphql({
+        query: `{
           brand(id:"${brandId}") {
             users {
               user {
                 imageUrl
                 id
                 fullName
-								email
-								
+				email			
               }
             }
           }
         }`,
-			});
-			// debugger;
-			if (team.data !== null && team.data.brand !== null) {
-				setTeam(team.data.brand.users);
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	};
+      });
+      if (team.data !== null && team.data.brand !== null) {
+        setTeam(team.data.brand.users);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	const handleRemoveMember = (index) => {
-		// debugger;
-		const mem = [...members]
-		const mem2 = [...TeamMembers]
-		mem2.splice(index, 1);
-		setMembers(mem2);
-	}
+  const handleRemoveMember = (index) => {};
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const closeHandle = () => {
+    setAddOpen(false);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  useEffect(() => {
+    getTeam();
+  }, []);
 
-	const open = Boolean(anchorEl);
-	const id = open ? 'simple-popover' : undefined;
+  debugger;
 
+  return (
+    <>
+      <AddMember open={addOpen} closeAdd={closeHandle} />
 
-	const handleTeam = () => {
-
-	}
-	const closeHandle = () => {
-		setAddOpen(false);
-	}
-
-	useEffect(() => {
-		
-	}, []);
-
-
-	useEffect(() => {
-		getTeam();
-	}, []);
-
-	debugger;
-
-	return (
-		<>
-			{/* {addCampaign && (
-        <AddCampaign
-          open={addCampaign}
-          handleCancel={() => setAddCampagin(false)}
-          brandId={brandId}
-        />
-      )} */}
-			<AddMember
-				open={addOpen}
-				closeAdd={closeHandle}
-			/>
-
-			<div className={styles.TeamContainer}>
-				<div className={styles.TeamHeadingContainer}>
-					<div className={styles.TeamHeading}>
-						<span>Team</span>
-					</div>
-					<button onClick={() => setAddOpen(true)}>
-						<AddIcon /> Add Member
+      <div className={styles.TeamContainer}>
+        <div className={styles.TeamHeadingContainer}>
+          <div className={styles.TeamHeading}>
+            <span>Team</span>
+          </div>
+          <button onClick={() => setAddOpen(true)}>
+            <AddIcon /> Add Member
           </button>
-				</div>
-			</div>
+        </div>
+      </div>
 
-			{teams.length == 0 ? (
-				<div>
-					<div className={styles.noMembers}>
-						<Grid alignItems="center">
-							<NoUser />
-						</Grid>
-					</div>
-					<div className={styles.noMembersText}>
-						<h6>
-							No Team Members Yet
-									</h6>
-						<p>
-							Add team or agency members to help you manage campaigns
-									</p>
-					</div>
-				</div>
-			) : (
-					<div className={styles.TeamInfoContainer}>
-						{teams && teams !== null ? (
-							teams.map((team, index) => (
-
-								<TeamData
-									TeamMembers={team}
-									index={index}
-									handleRemoveMember={handleRemoveMember} />
-							))
-
-						) : ('')
-						}
-					</div>
-				)
-			}
-		</>
-	);
+      {teams.length == 0 ? (
+        <div>
+          <div className={styles.noMembers}>
+            <Grid alignItems='center'>
+              <NoUser />
+            </Grid>
+          </div>
+          <div className={styles.noMembersText}>
+            <h6>No Team Members Yet</h6>
+            <p>Add team or agency members to help you manage campaigns</p>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.TeamInfoContainer}>
+          {teams && teams !== null
+            ? teams.map((team, index) => (
+                <TeamData
+                  TeamMembers={team}
+                  index={index}
+                  handleRemoveMember={handleRemoveMember}
+                />
+              ))
+            : ''}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Team;
