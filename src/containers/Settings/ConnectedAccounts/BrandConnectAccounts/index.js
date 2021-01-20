908b90f570ff19ca5a38148db5e2e1acb22233e4
ibmirzaib <ib.mirza.ib@gmail.com>
2021-01-21 03:33:49 +0500

    connected accounts integration

--- src/containers/Settings/ConnectedAccounts/BrandConnectAccounts/index.js
+++ src/containers/Settings/ConnectedAccounts/BrandConnectAccounts/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './BrandConnectAccounts.module.scss';
 import shopifyStoreLogo from './../../../../assets/shoppify.png';
 import TextField from '../../../../components/TextField';
@@ -7,145 +7,173 @@ import SyncIcon from '@material-ui/icons/Sync';
 import { InputAdornment, Grid } from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { API, graphqlOperation } from 'aws-amplify';
-
+import { RootContext } from '../../../../context/RootContext';
+import clsx from 'clsx';
 const BrandConnectAccounts = () => {
   const [storeName, setStoreName] = useState('');
   const [storeSaved, setStoreSaved] = useState(false);
   const [storeAccessToken, setStoreAccessToken] = useState('');
   const [storeApiKey, setStoreApiKey] = useState('');
-  const [storeBrandId, setStoreBrandId] = useState('');
   const [storeSharedKey, setStoreSharedKey] = useState('');
+  const { brandId } = useContext(RootContext);
+
+  useEffect(() => {
+    checkBrandConnection();
+  }, []);
+
+  const checkBrandConnection = async () => {
+    try {
+      const response = await API.graphql({
+        query: `{
+          brand(id:"${brandId}") {
+            plugins {
+              shopify {
+                shop
+              }
+            }
+          }
+        }`,
+      });
+      if (response.data.brand.plugins.shopify.shop) {
+        setStoreName(response.data.brand.plugins.shopify.shop);
+        setStoreSaved(true);
+      } else {
+        setStoreSaved(false);
+      }
+    } catch (e) {
+      setStoreSaved(false);
+    }
+  };
 
   const handleButtonClick = async (storeSaved) => {
     try {
-      console.log(storeSaved);
-      setStoreSaved(storeSaved);
-      if (!storeSaved) {
-        setStoreName('');
+      if (storeSaved) {
+        let data = {
+          shop: storeName,
+          accessToken: storeAccessToken,
+          apiKey: storeApiKey,
+          sharedKey: storeSharedKey,
+          brandId: brandId,
+        };
+        let registerResponse = await API.graphql(
+          graphqlOperation(
+            `mutation shopifyRegisterPrivate($input: ShopifyRegisterPrivateInput!) {
+				    shopifyRegisterPrivate(input: $input)
+		      }`,
+            {
+              input: data,
+            }
+          )
+        );
+        if (registerResponse.data.shopifyRegisterPrivate) {
+          setStoreSaved(true);
+        }
+      } else {
+        const disconnectResponse = await API.graphql(
+          graphqlOperation(
+            `mutation shopifyDisconnect($brandId: ID!){
+                shopifyDisconnect(brandId: $brandId)
+            }`,
+            {
+              brandId: brandId,
+            }
+          )
+        );
+        if (disconnectResponse.data.shopifyDisconnect) {
+          setStoreName('');
+          setStoreAccessToken('');
+          setStoreApiKey('');
+          setStoreSharedKey('');
+          setStoreSaved(false);
+        }
       }
-      let data = {
-        shop: storeName,
-        accessToken: storeAccessToken,
-        apiKey: storeApiKey,
-        sharedKey: storeSharedKey,
-        brandId: storeBrandId,
-      };
-      let response = await API.graphql(
-        graphqlOperation(
-          `mutation shopifyRegisterPrivate($input: shopifyRegisterPrivateInput!) {
-				shopifyRegisterPrivate(input: $input)
-		  }
-		  `,
-          {
-            input: data,
-          }
-        )
-      );
     } catch (e) {}
   };
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.contentContainer}>
-        <Grid container spacing={3}>
-          <p>
-            Connecting your shopify account allows us to populate your products
-            so you can create successful campaigns
-          </p>
-          <Grid item xs={12}>
-            <img
-              className={styles.shopifyLogo}
-              src={shopifyStoreLogo}
-              alt='Shopify Logo'
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id='outlined-basic'
-              fullWidth
-              label='Shopify Shop Name'
-              variant={storeSaved ? 'filled' : 'outlined'}
-              value={storeName}
-              onChange={(e) => {
-                setStoreName(e.target.value);
-              }}
-              disabled={storeSaved ? true : false}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment
-                    className={styles.inputendornment}
-                    position='end'
-                  >
-                    {storeSaved ? (
-                      <CheckCircleIcon
-                        className={styles.inputendornmentCheck}
-                      />
-                    ) : (
-                      ''
-                    )}
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id='outlined-basic'
-              fullWidth
-              value={storeAccessToken}
-              onChange={(e) => {
-                setStoreAccessToken(e.target.value);
-              }}
-              label='Access Token'
-              variant='outlined'
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id='outlined-basic'
-              fullWidth
-              value={storeApiKey}
-              onChange={(e) => {
-                setStoreApiKey(e.target.value);
-              }}
-              label='API Key'
-              variant='outlined'
-            />
-          </Grid>
-
-          <Grid item xs={6}>
-            <TextField
-              id='outlined-basic'
-              fullWidth
-              value={storeBrandId}
-              onChange={(e) => {
-                setStoreBrandId(e.target.value);
-              }}
-              label='Brand Id'
-              variant='outlined'
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id='outlined-basic'
-              fullWidth
-              value={storeSharedKey}
-              onChange={(e) => {
-                setStoreSharedKey(e.target.value);
-              }}
-              label='Shared Key'
-              variant='outlined'
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <ChipButton
-              title={storeSaved ? 'Clear' : 'Connect'}
-              buttonSize={'sm'}
-              handleClick={() => handleButtonClick(!storeSaved)}
-            />
-          </Grid>
-
+        <p>
+          Connecting your shopify account allows us to populate your products so
+          you can create successful campaigns
+        </p>
+        <img
+          className={styles.shopifyLogo}
+          src={shopifyStoreLogo}
+          alt='Shopify Logo'
+        />
+        <div
+          className={clsx(
+            storeSaved ? styles.savedContainer : '',
+            styles.fieldsAndAction
+          )}
+        >
+          <TextField
+            id='outlined-basic'
+            fullWidth
+            label='Shopify Shop Name'
+            variant={storeSaved ? 'filled' : 'outlined'}
+            value={storeName}
+            onChange={(e) => {
+              setStoreName(e.target.value);
+            }}
+            disabled={storeSaved ? true : false}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment
+                  className={styles.inputendornment}
+                  position='end'
+                >
+                  {storeSaved ? (
+                    <CheckCircleIcon className={styles.inputendornmentCheck} />
+                  ) : (
+                    ''
+                  )}
+                </InputAdornment>
+              ),
+            }}
+          />
+          {storeSaved ? (
+            ''
+          ) : (
+            <>
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                value={storeAccessToken}
+                onChange={(e) => {
+                  setStoreAccessToken(e.target.value);
+                }}
+                label='Access Token'
+                variant='outlined'
+              />
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                value={storeApiKey}
+                onChange={(e) => {
+                  setStoreApiKey(e.target.value);
+                }}
+                label='API Key'
+                variant='outlined'
+              />
+              <TextField
+                id='outlined-basic'
+                fullWidth
+                value={storeSharedKey}
+                onChange={(e) => {
+                  setStoreSharedKey(e.target.value);
+                }}
+                label='Shared Key'
+                variant='outlined'
+              />
+            </>
+          )}
+          <ChipButton
+            title={storeSaved ? 'Clear' : 'Connect'}
+            buttonSize={'sm'}
+            handleClick={() => handleButtonClick(!storeSaved)}
+          />
           {storeSaved ? (
             <div className={styles.storeSync}>
               <SyncIcon />
@@ -154,7 +182,7 @@ const BrandConnectAccounts = () => {
           ) : (
             ''
           )}
-        </Grid>
+        </div>
       </div>
     </div>
   );
