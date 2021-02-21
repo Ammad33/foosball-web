5b8feae63a43ba8b0a48a8acd8654d8408eaa627
aqeeldilmir <aqeeldilmir@gmail.com>
2021-02-21 12:18:47 +0500

    name change

--- src/containers/CampaignDetail/InviteCard/index.js
+++ src/containers/CampaignDetail/InviteCard/index.js
@@ -1,19 +1,19 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './InviteCard.module.scss';
 import DeclineDialog from '../../../components/CancellationDialog';
 import Translation from '../../../assets/translation.json';
 import { API, graphqlOperation } from 'aws-amplify';
-import {RootContext} from '../../../context/RootContext';
+import { RootContext } from '../../../context/RootContext';
 
 
 
-const InviteCard = ({createdBy, campaignId, handleStatus, invitationMessage , handleReviewAndSign}) => {
+const InviteCard = ({ createdBy, campaignId, handleStatus, invitationMessage, handleReviewAndSign }) => {
 	const [decline, setDecline] = useState(false);
-	const [declineReason , setDeclineReason] = useState('');
-	const [reasonDetail , setReasonDetail] = useState('');
+	const [declineReason, setDeclineReason] = useState('');
+	const [reasonDetail, setReasonDetail] = useState('');
 	const {
-   brandId
-  } = useContext(RootContext);
+		brandId
+	} = useContext(RootContext);
 
 
 	const reasons = [
@@ -50,8 +50,8 @@ const InviteCard = ({createdBy, campaignId, handleStatus, invitationMessage , ha
 				)
 			)
 			acceptCampaignTerms();
-		}	
-		catch(e) {
+		}
+		catch (e) {
 			console.log("Error in accepting invite", e)
 		}
 	}
@@ -59,8 +59,8 @@ const InviteCard = ({createdBy, campaignId, handleStatus, invitationMessage , ha
 
 	const acceptCampaignTerms = async () => {
 		try {
-			await API.graphql (
-				graphqlOperation (
+			await API.graphql(
+				graphqlOperation(
 					`mutation AcceptCampaignTerms {
 						influencerAcceptCampaignTerms(input: {
 							campaignId: "${campaignId}",
@@ -71,35 +71,35 @@ const InviteCard = ({createdBy, campaignId, handleStatus, invitationMessage , ha
 			)
 		}
 		catch (e) {
-			console.log("Error in accepting campaign terms " , e)
+			console.log("Error in accepting campaign terms ", e)
 		}
 	}
 
 
-    return (
-			<>
-			<DeclineDialog 
-			open={decline} 
-			handleClose={() => setDecline(false)} 
-			reason={declineReason} 
-			reasons = {reasons}
-			handleReason = {handleDeclineReason} 
-			message={Translation.DIALOG.CAMPAIGN_DECLINE_DIALOG_MSG}
-			buttonText = "Decline"
-			handleReasonDetail = {handleReasonDetail}
-			reasonDetail = {reasonDetail}
+	return (
+		<>
+			<DeclineDialog
+				open={decline}
+				handleClose={() => setDecline(false)}
+				reason={declineReason}
+				reasons={reasons}
+				handleReason={handleDeclineReason}
+				message={Translation.DIALOG.CAMPAIGN_DECLINE_DIALOG_MSG}
+				buttonText="Decline"
+				handleReasonDetail={handleReasonDetail}
+				reasonDetail={reasonDetail}
 
 			/>
-        <div className={styles.declineContainer}>
-            <h1>{createdBy.name} has inivted you to a campaign</h1>
-            <p className={styles.firstp}>{invitationMessage}</p>
-            <p className={styles.secondp}></p>
-            <div className={styles.buttonContainer}>
-                <button className={styles.accept} onClick={()=>handleAcceptInvite()} >Accept</button>
-                <button className={styles.nego}>Negotiate</button>
-                <button className={styles.decline} onClick={() => setDecline(true)} >Decline</button>
-            </div>
-        </div> </>)
+			<div className={styles.declineContainer}>
+				<h1>{createdBy.name} has invited you to a campaign</h1>
+				<p className={styles.firstp}>{invitationMessage}</p>
+				<p className={styles.secondp}></p>
+				<div className={styles.buttonContainer}>
+					<button className={styles.accept} onClick={() => handleAcceptInvite()} >Accept</button>
+					<button className={styles.nego}>Negotiate</button>
+					<button className={styles.decline} onClick={() => setDecline(true)} >Decline</button>
+				</div>
+			</div> </>)
 };
 
 export default InviteCard
